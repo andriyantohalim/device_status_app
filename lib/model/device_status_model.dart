5fad717e8e2ec31d231bce177da3021d9154b74e
Andriyanto Halim <andriyanto.halim@gmail.com>
2024-07-17 21:51:25 +0800

    more battery information

--- lib/model/device_status_model.dart
+++ lib/model/device_status_model.dart
@@ -1,6 +1,15 @@
 class DeviceStatus {
   final int batteryLevel;
   final String batteryHealth;
+  final int batteryTemperature;
+  final int batteryVoltage;
+  final String batteryStatus;
 
-  DeviceStatus({required this.batteryLevel, required this.batteryHealth});
+  DeviceStatus({
+    required this.batteryLevel,
+    required this.batteryHealth,
+    required this.batteryTemperature,
+    required this.batteryVoltage,
+    required this.batteryStatus,
+  });
 }
